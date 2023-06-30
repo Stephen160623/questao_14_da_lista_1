bd888c5ba9b90e06d012ccaa59a50542eb74618b
Stephen160623 <136847798+Stephen160623@users.noreply.github.com>
2023-06-30 14:20:47 -0300

    Add files via upload

---
+++ questao1.14.py
@@ -0,0 +1,16 @@
+# Atribuição de variáveis
+   
+# Entrada de dados
+
+x1 = float(input("x1: "))
+y1 = float(input("y1: "))
+x2 = float(input("x2: "))
+y2 = float(input("y2: "))
+
+#Processamento de dados
+
+d = ((x2-x1)**2+(y2-y1)**2)**0.5
+
+# Saída de dados
+
+print("A distância entre os dois pontos é: "  + str(d))
